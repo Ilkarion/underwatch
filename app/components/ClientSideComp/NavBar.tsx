b670b6f56b336079d44d1975d614a561216f0cf3
Illia <122636254+Ilkarion@users.noreply.github.com>
2025-08-16 15:46:23 +0300

    Update NavBar.tsx

--- app/components/ClientSideComp/NavBar.tsx
+++ app/components/ClientSideComp/NavBar.tsx
@@ -27,7 +27,7 @@ export default function NavBar() {
 
     return(
         <>
-            <div className={` flex fixed z-1 flex-row items-center justify-between pr-[5%] pl-[5%] w-full transform transition-all ease-out delay-50 ${scrolled ? 'h-[60px] bg-white' : 'h-[75px] bg-transparent'} 2xl:h-[100px] max-[500px]:hidden`}>
+            <div className={` flex fixed z-100 flex-row items-center justify-between pr-[5%] pl-[5%] w-full transform transition-all ease-out delay-50 ${scrolled ? 'h-[60px] bg-white' : 'h-[75px] bg-transparent'} 2xl:h-[100px] max-[500px]:hidden`}>
                 <Image src={Logo} alt="Logo blue" className='w-[5vw]' priority={true}/>
                 <div className={`flex flex-row gap-[3vw] text-[1.2vw] font-light ${scrolled ? 'text-black' : 'text-white'}`}>
                     <div className={`about border pl-[0.8vw] pr-[0.8vw] pt-[0.4vw] pb-[0.4vw] ${scrolled ? 'border-black transition ease-in-out delay-150 hover:border-gray-600 hover:text-gray-600' : 'border-white transition ease-in-out delay-150 hover:border-[#E5E7EB] hover:text-[#E5E7EB]'} `}><Link href="/">ABOUT US</Link></div>
